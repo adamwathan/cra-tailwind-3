e9d5b316ebfba08d74b90725e91cd3673ac5265e
Adam Wathan <adam.wathan@gmail.com>
2021-12-04 20:09:54 -0500

    Setup Tailwind CLI

--- src/App.js
+++ src/App.js
@@ -1,23 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="min-h-screen grid place-items-center">
+      <h1 className="text-4xl font-bold">Create React App + Tailwind 3 = 💏</h1>
     </div>
   );
 }
